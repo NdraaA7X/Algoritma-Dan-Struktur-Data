5cc96b31bb56cb0ede2b02a2c55d1a59856472ba
Hendra <erhanhendragianpermana@gmail.com>
2026-03-15 21:28:13 +0700

    Praktikum

--- jobsheet5/Pangkat10.java
+++ jobsheet5/Pangkat10.java
@@ -16,11 +16,16 @@ public class Pangkat10 {
         return hasil;
     }
 
-    int pnagkatDC(int a, int n) {
-        int hasil = 1;
-        for(int i = 0; i < n; i++) {
-            hasil = hasil * a;
+    int pangkatDC(int a, int n) {
+        if (n==1) {
+            return a;
+        }else {
+            if (n%2==1) {
+                return (pangkatDC(a, n/2)*pangkatDC(a, n/2)*a);
+            }else {
+                return (pangkatDC(a, n/2)*pangkatDC(a, n/2));
+            }
         }
-        return hasil;
     }
+
 }
