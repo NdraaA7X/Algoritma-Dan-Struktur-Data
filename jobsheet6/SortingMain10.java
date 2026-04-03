4139e0fed8d06eeec4fd411a0cf8a81e41c62f66
Hendra <erhanhendragianpermana@gmail.com>
2026-04-03 13:48:42 +0700

    Praktikum

--- jobsheet6/SortingMain10.java
+++ jobsheet6/SortingMain10.java
@@ -1,15 +1,14 @@
 package jobsheet6;
 
 public class SortingMain10 {
-    
-    int a[] = {20, 10, 2, 7, 12};
-    shorting10 dataurut1 = new shorting10(a, a.length);
-
-    public SortingMain10() {
-        System.out.println("Data Awal 1");
-        dataurut1.tampil();
-        dataurut1.bubbleSort();
-        System.out.println("Data Sudah Diurutkan dengan BUBBLE SORT (ASC)");
-        dataurut1.tampil();
+    public static void main(String[] args) {
+        int a[] = {20, 10, 2, 7, 12};
+        shorting10 dataurut1 = new shorting10(a, a.length);
+            System.out.println("Data Awal 1");
+            dataurut1.tampil();
+            dataurut1.bubbleSort();
+            System.out.println("Data Sudah Diurutkan dengan BUBBLE SORT (ASC)");
+            dataurut1.tampil();        
     }
 }
+
