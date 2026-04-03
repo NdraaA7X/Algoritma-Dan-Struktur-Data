4d88014e4b4667078d25187113187b969329acf9
Hendra <erhanhendragianpermana@gmail.com>
2026-04-03 15:27:10 +0700

    Praktikum

--- jobsheet6/MahasiswaBerprestasi10.java
+++ jobsheet6/MahasiswaBerprestasi10.java
@@ -1,5 +1,4 @@
 package jobsheet6;
-import java.util.Scanner;
 
 public class MahasiswaBerprestasi10 {
     Mahasiswa10[] listMhs = new Mahasiswa10[5];
@@ -31,39 +30,5 @@ public class MahasiswaBerprestasi10 {
                 }
             }
         }
-    }
-
-    void insertionSortDesc() {
-        for (int i = 1; i < listMhs.length; i++) {
-            Mahasiswa10 temp = listMhs[i];
-            int j = i;
-            while (j > 0 && listMhs[j - 1].ipk < temp.ipk) {
-                listMhs[j] = listMhs[j - 1];
-                j--;
-            }
-            listMhs[j] = temp;
-        }
-    }
-
-    void tambahDinamis() {
-        Scanner sc = new Scanner(System.in);
-        if (idx < listMhs.length) {
-            System.out.println("Masukkan Data Mahasiswa ke-" + (idx + 1));
-            System.out.print("NIM    : ");
-            String nim   = sc.nextLine();
-            System.out.print("Nama   : ");
-            String nama  = sc.nextLine();
-            System.out.print("Kelas  : ");
-            String kelas = sc.nextLine();
-            System.out.print("IPK    : ");
-            double ipk   = Double.parseDouble(sc.nextLine());
-            System.out.println("------------------------------");
-            listMhs[idx] = new Mahasiswa10(nim, nama, kelas, ipk);
-            idx++;
-        } else {
-            System.out.println("data sudah penuh");
-        }
-
-        sc.close();
     }
 }
