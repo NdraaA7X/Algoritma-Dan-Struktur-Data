195576fe20970cf9a4b02cbfa611465c46472bca
Hendra <erhanhendragianpermana@gmail.com>
2026-04-09 16:22:44 +0700

    Praktikum

---
+++ jobsheet7/MahasiswaDemo10.java
@@ -0,0 +1,43 @@
+package jobsheet7;
+import java.util.Scanner;
+
+public class MahasiswaDemo10 {
+    public static void main(String[] args) {
+        MahasiswaBerprestasi10 list = new MahasiswaBerprestasi10();
+        Scanner sc = new Scanner(System.in);
+        int jumMhs = 5;
+ 
+       for (int i = 0; i < jumMhs; i++) {
+        System.out.println("Masukan Data Mahasiswa ke-" + (i + 1));
+        System.out.print("NIM   : ");
+        String nim = sc.nextLine();
+        System.out.print("NAMA  : ");
+        String nama = sc.nextLine();
+        System.out.print("KELAS : ");
+        String kelas = sc.nextLine();
+        System.out.print("IPK   : ");
+        String ip = sc.nextLine();
+        double ipk = Double.parseDouble(ip);
+        System.out.println("-------------------------------");
+        list.tambah(new Mahasiswa10(nim, nama, kelas, ipk));
+       }
+
+        list.tampil();
+        System.out.println("-----------------------------------------");
+        System.out.println("Pencarian data");
+        System.out.println("-----------------------------------------");
+        System.out.println("Masukkan ipk mahasiswa yang dicari: ");
+        System.out.print("IPK : ");
+        double cari = sc.nextDouble();
+
+        System.out.println("------------------------------------------");
+        System.out.println("menggunakan binary search");
+        System.out.println("------------------------------------------");
+        double posisi2 = list.findBinarySearch(cari, 0, jumMhs - 1);
+        int pss2 = (int)posisi2;
+        list.tampilPosisi(cari, pss2);
+        list.tampilDataSearch(cari, pss2);
+
+        sc.close();
+    }
+}
