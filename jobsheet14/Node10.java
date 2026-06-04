ac1038c8f6623941a4f01302bcde1ecbb49e51c0
Hendra <erhanhendragianpermana@gmail.com>
2026-06-04 22:35:06 +0700

    Praktikum

---
+++ jobsheet14/Node10.java
@@ -0,0 +1,14 @@
+package jobsheet14;
+
+public class Node10 {
+    Mahasiswa10 mahasiswa;
+    Node10 left;
+    Node10 right;
+
+    public Node10() {}
+
+    public Node10(Mahasiswa10 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }
+}
